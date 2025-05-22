1619f48cd74bcfe8f28e99ddaae00f3964c033ac
ayumuOhara <kmm2459310@stu.o-hara.ac.jp>
2025-05-22 16:33:17 +0900

    Update GameManager.cs

--- Battle/Assets/Scripts/GameManager.cs
+++ Battle/Assets/Scripts/GameManager.cs
@@ -14,38 +14,8 @@ public class GameManager : MonoBehaviourPunCallbacks
     {
         if (!hasSpawned && PhotonNetwork.CurrentRoom != null && PhotonNetwork.CurrentRoom.PlayerCount == 4)
         {
-            if (PhotonNetwork.IsMasterClient)
-            {
-                SpawnAllPlayers();
-            }
+            PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity);
             hasSpawned = true;
-        }
-    }
-
-    void SpawnAllPlayers()
-    {
-        // スポーン位置をシャッフル
-        List<Transform> shuffledSpawnPoints = new List<Transform>(spawnList);
-        Shuffle(shuffledSpawnPoints);
-
-        int index = 0;
-        foreach (Player player in PhotonNetwork.PlayerList)
-        {
-            object[] instantiationData = new object[] { index }; // インデックスを渡して各クライアントが自分で使う
-            PhotonNetwork.Instantiate("Player", shuffledSpawnPoints[index].position, Quaternion.identity, 0, instantiationData);
-            index++;
-        }
-    }
-
-    // リストをシャッフルするユーティリティ
-    void Shuffle<T>(List<T> list)
-    {
-        for (int i = 0; i < list.Count; i++)
-        {
-            int rnd = Random.Range(i, list.Count);
-            T temp = list[i];
-            list[i] = list[rnd];
-            list[rnd] = temp;
         }
     }
 }
